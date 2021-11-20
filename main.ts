ca4621bb2f528326dd20916089c781c302a2c9a7
boeing7478F <85016918+boeing7478F@users.noreply.github.com>
2021-11-20 21:05:39 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let list: number[] = []
+serial.redirect(
+SerialPin.USB_TX,
+SerialPin.USB_RX,
+BaudRate.BaudRate115200
+)
+serial.writeLine("temperature, light, acceleration, direction")
+basic.forever(function () {
+    serial.writeNumbers([
+    input.temperature(),
+    input.lightLevel(),
+    input.acceleration(Dimension.Strength),
+    input.compassHeading()
+    ])
+    for (let index = 0; index <= list[index]; index++) {
+        basic.showNumber(list[index])
+        basic.showString("Hello!")
+    }
+})
